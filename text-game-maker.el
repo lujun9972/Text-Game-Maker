2322a0eef9b8479987ff5c2208f9370c8e15f9a3
lujun9972 <lujun9972@gmail.com>
2015-07-18 20:15:20 +0800

    增加一些基础函数

--- text-game-maker.el
+++ text-game-maker.el
@@ -1,6 +1,14 @@
+(defun file-content(file)
+  "返回file的文件内容"
+  (with-temp-buffer
+	(insert-file-contents file)
+	(buffer-string)))
 (require 'tg-mode)
+(defvar display-fn #'tg-mprinc
+  "显示信息的函数")
 (defun tg-display (&rest args)
   (apply display-fn args))
+(require 'eieio)
 (require 'room-maker)
 (require 'inventory-maker)
 (require 'creature-maker)
